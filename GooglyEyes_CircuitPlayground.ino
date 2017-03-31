f7f770f911966f89d57924a417961ff32a389fc9
John <cutlasjgh@users.noreply.github.com>
2017-03-30 22:24:26 -0400

    adding initial 3 files

---
+++ GooglyEyes_CircuitPlayground.ino
@@ -0,0 +1,299 @@
+
+
+// Googly Eye Goggles
+// By Bill Earl
+// For Adafruit Industries
+// from https://learn.adafruit.com/steam-punk-goggles/overview?view=all
+// cktplayground contains this accelerometer https://learn.adafruit.com/adafruit-lis3dh-triple-axis-accelerometer-breakout?view=all
+//
+// The googly eye effect is based on a physical model of a pendulum.
+// The pendulum motion is driven by accelerations in 2 axis.
+// Eye color varies with orientation of the magnetometer
+
+//rem'd out the magnetometer use
+//now replace LSM303 with built in circuit playground gyro
+
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+//#include <Adafruit_LSM303_U.h>
+#include <Adafruit_CircuitPlayground.h>
+#include <Adafruit_NeoPixel.h>
+
+#define neoPixelPin 10
+
+// We could do this as 2 16-pixel rings wired in parallel.
+// But keeping them separate lets us do the right and left
+// eyes separately if we want.
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(32, neoPixelPin, NEO_GRB + NEO_KHZ800);
+
+//Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(54321);
+// adafruit unified does not seem to support the lis accel found on cktplayground
+//Adafruit_LSM303_Mag_Unified mag = Adafruit_LSM303_Mag_Unified(12345);
+ 
+float pos = 8;  // Starting center position of pupil
+float increment = 2 * 3.14159 / 16; // distance between pixels in radians
+float MomentumH = 0; // horizontal component of pupil rotational inertia
+float MomentumV = 0; // vertical component of pupil rotational inertia
+
+// Tuning constants. (a.k.a. "Fudge Factors)  
+// These can be tweaked to adjust the liveliness and sensitivity of the eyes.
+const float friction = 0.995; // frictional damping constant.  1.0 is no friction.
+const float swing = 60;  // arbitrary divisor for gravitational force
+const float gravity = 200;  // arbitrary divisor for lateral acceleration
+const float nod = 7.5; // accelerometer threshold for toggling modes
+
+long nodStart = 0;
+long nodTime = 2000;
+
+bool antiGravity = false;  // The pendulum will anti-gravitate to the top.
+bool mirroredEyes = false; // The left eye will mirror the right.
+
+const float halfWidth = 1.25; // half-width of pupil (in pixels)
+
+// Pi for calculations - not the raspberry type
+const float Pi = 3.14159;
+
+void setup(void) 
+{
+   strip.begin();
+   strip.show(); // Initialize all pixels to 'off'  sensor_t sensor;
+
+   // Initialize the sensors
+   //accel.begin();
+   //mag.begin();
+   //I think we can do cktplayground.begin() instead of just accel.begin()
+  CircuitPlayground.begin();
+   
+   resetModes();
+}
+
+// main processing loop
+void loop(void) 
+{
+   // Read the magnetometer and determine the compass heading:
+   sensors_event_t event; 
+   //mag.getEvent(&event);
+
+   // Calculate the angle of the vector y,x from magnetic North
+   //float heading = (atan2(event.magnetic.y,event.magnetic.x) * 180) / Pi;
+   float heading = 0.0; 
+
+   // Normalize to 0-360 for a compass heading
+   if (heading < 0)
+   {
+      heading = 360 + heading;
+   }
+
+   // Now read the accelerometer to control the motion.
+   CircuitPlayground.lis.getEvent(&event);
+
+   // Check for mode change commands
+   CheckForNods(event);
+
+   // apply a little frictional damping to keep things in control and prevent perpetual motion
+   MomentumH *= friction;
+   MomentumV *= friction;
+
+   // Calculate the horizontal and vertical effect on the virtual pendulum
+   // 'pos' is a pixel address, so we multiply by 'increment' to get radians.
+   float TorqueH = cos(pos * increment);  // peaks at top and bottom of the swing
+   float TorqueV = sin(pos * increment);    // peaks when the pendulum is horizontal
+
+   // Add the incremental acceleration to the existing momentum
+   // This code assumes that the accelerometer is mounted upside-down, level
+   // and with the X-axis pointed forward.  So the Y axis reads the horizontal
+   // acceleration and the inverse of the Z axis is gravity.
+   // For other orientations of the sensor, just change the axis to match.
+   MomentumH += TorqueH * event.acceleration.y / swing;
+   if (antiGravity)
+   {
+     MomentumV += TorqueV * event.acceleration.z / gravity;
+   }
+   else
+   {
+     MomentumV -= TorqueV * event.acceleration.z / gravity;
+   }
+
+   // Calculate the new position
+   pos += MomentumH + MomentumV;
+   
+   // handle the wrap-arounds at the top
+   while (round(pos) < 0) pos += 16.0;
+   while (round(pos) > 15) pos -= 16.0;
+
+   // Now re-compute the display
+   for (int i = 0; i < 16; i++)
+   {
+      // Compute the distance bewteen the pixel and the center
+      // point of the virtual pendulum.
+      float diff = i - pos;
+
+      // Light up nearby pixels proportional to their proximity to 'pos'
+      if (fabs(diff) <= halfWidth) 
+      {
+         uint32_t color;
+         float proximity = halfWidth - fabs(diff) * 200;
+
+         // pick a color based on heading & proximity to 'pos'
+         color = selectColor(heading, proximity);
+         
+         // do both eyes
+         strip.setPixelColor(i, color);
+         if (mirroredEyes)
+         {
+           strip.setPixelColor(31 - i, color);
+         }
+         else
+         {
+           strip.setPixelColor(i + 16, color);
+         }
+      }
+      else // all others are off
+      {
+         strip.setPixelColor(i, 0);
+         if (mirroredEyes)
+         {
+           strip.setPixelColor(31 - i, 0);
+         }
+         else
+         {
+           strip.setPixelColor(i + 16, 0);
+         }
+      }
+   }
+   // Now show it!
+   strip.show();
+}
+
+// choose a color based on the compass heading and proximity to "pos".
+uint32_t selectColor(float heading, float proximity)
+{
+     uint32_t color;
+
+     // Choose eye color based on the compass heading
+     if (heading < 60)
+     {
+        color = strip.Color(0, 0, proximity);
+     }
+     else if (heading < 120)
+     {
+        color = strip.Color(0, proximity, proximity);
+     }
+     else if (heading < 180)
+     {
+        color = strip.Color(0, proximity, 0);
+     }
+     else if (heading < 240)
+     {
+        color = strip.Color(proximity, proximity, 0);
+     }
+     else if (heading < 300)
+     {
+        color = strip.Color(proximity, 0, 0);
+     }
+     else // 300-360
+     {
+        color = strip.Color(proximity, 0, proximity);
+     }
+}
+
+// monitor orientation for mode-change 'gestures'
+void CheckForNods(sensors_event_t event)
+{
+   if (event.acceleration.x > nod)
+   {
+     if (millis() - nodStart > nodTime)
+     {
+       antiGravity = false;  
+       nodStart = millis(); // reset timer     
+       spinDown();
+     }
+   }
+   else if (event.acceleration.x < -(nod + 1))
+   {
+     if (millis() - nodStart > nodTime)
+     {
+       antiGravity = true;  
+       spinUp();
+       nodStart = millis(); // reset timer     
+     }
+   }
+   else if (event.acceleration.y > nod)
+   {
+     if (millis() - nodStart > nodTime)
+     {
+       mirroredEyes = false;  
+       spinDown();
+       nodStart = millis(); // reset timer     
+     }
+   }
+   else if (event.acceleration.y < -nod)
+   {
+     if (millis() - nodStart > nodTime)
+     {
+       mirroredEyes = true;  
+       spinUp();
+       nodStart = millis(); // reset timer     
+      }
+   }
+   else // no nods in progress
+   {
+     nodStart = millis(); // reset timer
+   }
+}
+
+// Reset to default
+void resetModes()
+{
+   antiGravity = false;
+   mirroredEyes = false;
+   
+   /// spin-up
+   spin(strip.Color(255,0,0), 1, 500);
+   spin(strip.Color(0,255,0), 1, 500);
+   spin(strip.Color(0,0,255), 1, 500);
+   spinUp();
+}
+
+// gradual spin up
+void spinUp()
+{
+   for (int i = 300; i > 0;  i -= 20)
+   {
+     spin(strip.Color(255,255,255), 1, i);
+   }
+   pos = 0;
+   // leave it with some momentum and let it 'coast' to a stop
+   MomentumH = 3;  
+}
+
+// Gradual spin down
+void spinDown()
+{
+   for (int i = 1; i < 300; i++)
+   {
+     spin(strip.Color(255,255,255), 1, i += 20);
+   }
+   // Stop it dead at the top and let it swing to the bottom on its own
+   pos = 0;
+   MomentumH = MomentumV = 0;
+}
+
+
+// utility function for feedback on mode changes.
+void spin(uint32_t color, int count, int time)
+{
+  for (int j = 0; j < count; j++)
+  {
+    for (int i = 0; i < 16; i++)
+    {
+      strip.setPixelColor(i, color);
+      strip.setPixelColor(31 - i, color);
+      strip.show();
+      delay(max(time / 16, 1));
+      strip.setPixelColor(i, 0);
+      strip.setPixelColor(31 - i, 0);
+      strip.show();
+    }
+  }
+}
---
+++ SimpleGoggle.ino
@@ -0,0 +1,352 @@
+// Not so simple NOW, just not googly eyed..
+// added modes from RGBWstrandtest (neopixel example code)
+//adding mode switch on tap (accelerometer input) instead of timed
+
+#include <Adafruit_CircuitPlayground.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR_ATtiny85__ // Trinket, Gemma, etc.
+ #include <avr/power.h>
+#endif
+
+#define PIN 10
+#define NUM_LEDS 32
+#define BRIGHTNESS 15
+#define MAXMODES 3
+
+// Adjust this number for the sensitivity of the 'click' force
+// this strongly depend on the range! for 16G, try 5-10
+// for 8G, try 10-20. for 4G try 20-40. for 2G try 40-80
+#define CLICKTHRESHHOLD 120 // 40 is very  sensitive, 120 less sensitive
+
+//Adafruit_NeoPixel pixels = Adafruit_NeoPixel(32, PIN);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRBW + NEO_KHZ800);
+
+uint8_t  mode   = 1, // Starting animation effect
+         offset = 0; // Position of spinny eyes
+uint32_t color  = 0xFF0000; // Start red
+uint32_t prevTime;
+
+
+int gamma[] = {
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
+    1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
+    2,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  5,  5,  5,
+    5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9, 10,
+   10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
+   17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
+   25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
+   37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
+   51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
+   69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
+   90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114,
+  115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
+  144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
+  177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
+  215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255 };
+
+
+void tapTime(void) {
+  // do something :)
+   //Serial.print("Tap! ");
+   //Serial.println(millis()); // the time
+   int i;
+    mode++;                        // Next mode
+    if(mode > MAXMODES) {                 // End of modes?
+      mode = 0;                    // Start modes over
+      color >>= 8;                 // Next color R->G->B
+      if(!color) color = 0xFF0000; // Reset to red
+    }
+    for(i=0; i<32; i++) strip.setPixelColor(i, 0);
+}
+
+void setup() {
+#ifdef __AVR_ATtiny85__ // Trinket, Gemma, etc.
+  if(F_CPU == 16000000) clock_prescale_set(clock_div_1);
+#endif
+
+  //cktplayground for tap sensing
+  CircuitPlayground.begin();
+  CircuitPlayground.setAccelRange(LIS3DH_RANGE_2_G);   // 2, 4, 8 or 16 G!
+  // 0 = turn off click detection & interrupt
+  // 1 = single click only interrupt output
+  // 2 = double click only interrupt output, detect single click
+  // Adjust threshhold, higher numbers are less sensitive
+  CircuitPlayground.setAccelTap(1, CLICKTHRESHHOLD);
+  // have a procedure called when a tap is detected
+  attachInterrupt(digitalPinToInterrupt(7), tapTime, FALLING);
+  
+  strip.setBrightness(BRIGHTNESS); // 1/3 brightness = 85
+  strip.begin();
+  prevTime = millis();
+}
+
+
+
+void loop() {
+  uint8_t  i;
+  uint32_t t;
+
+  switch(mode) {
+
+   case 0: // Random sparks - just one LED on at a time!
+    i = random(32);
+    strip.setPixelColor(i, color);
+    strip.show();
+    delay(10); //milliseconds
+    strip.setPixelColor(i, 0);
+    break;
+ 
+   case 1: // Spinny wheels (8 LEDs on at a time)
+    for(i=0; i<16; i++) {
+      uint32_t c = 0;
+      if(((offset + i) & 7) < 2) c = color; // 4 pixels on...
+      strip.setPixelColor(   i, c); // First eye
+      strip.setPixelColor(31-i, c); // Second eye (flipped)
+    }
+    strip.show();
+    offset++;
+    delay(80);
+    break;
+    
+    case 2: // 
+      rainbowFade2White(3,3,1);
+    break;
+    
+    case 3: //
+      colorWipe(strip.Color(60, 0, 60), 50); // Purple
+      colorWipe(strip.Color(250, 0, 250), 50); // Purple
+      colorWipe(strip.Color(60, 0, 255), 50); // Purpleblue
+    break;
+    
+   
+  }
+
+  t = millis();
+  
+  #if 0
+
+  //if not using tapsense then do timed mode change
+  //every 15 secs change mode
+  if((t - prevTime) > 15000) {      // Every 15 seconds...
+    mode++;                        // Next mode
+    if(mode > MAXMODES) {                 // End of modes?
+      mode = 0;                    // Start modes over
+      color >>= 8;                 // Next color R->G->B
+      if(!color) color = 0xFF0000; // Reset to red
+    }
+    for(i=0; i<32; i++) strip.setPixelColor(i, 0);
+    prevTime = t;
+  }
+  #endif
+
+  
+}
+
+
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
+}
+
+void pulseWhite(uint8_t wait) {
+  for(int j = 0; j < 256 ; j++){
+      for(uint16_t i=0; i<strip.numPixels(); i++) {
+          strip.setPixelColor(i, strip.Color(0,0,0, gamma[j] ) );
+        }
+        delay(wait);
+        strip.show();
+      }
+
+  for(int j = 255; j >= 0 ; j--){
+      for(uint16_t i=0; i<strip.numPixels(); i++) {
+          strip.setPixelColor(i, strip.Color(0,0,0, gamma[j] ) );
+        }
+        delay(wait);
+        strip.show();
+      }
+}
+
+
+void rainbowFade2White(uint8_t wait, int rainbowLoops, int whiteLoops) {
+  float fadeMax = 100.0;
+  int fadeVal = 0;
+  uint32_t wheelVal;
+  int redVal, greenVal, blueVal;
+
+  for(int k = 0 ; k < rainbowLoops ; k ++){
+    
+    for(int j=0; j<256; j++) { // 5 cycles of all colors on wheel
+
+      for(int i=0; i< strip.numPixels(); i++) {
+
+        wheelVal = Wheel(((i * 256 / strip.numPixels()) + j) & 255);
+
+        redVal = red(wheelVal) * float(fadeVal/fadeMax);
+        greenVal = green(wheelVal) * float(fadeVal/fadeMax);
+        blueVal = blue(wheelVal) * float(fadeVal/fadeMax);
+
+        strip.setPixelColor( i, strip.Color( redVal, greenVal, blueVal ) );
+
+      }
+
+      //First loop, fade in!
+      if(k == 0 && fadeVal < fadeMax-1) {
+          fadeVal++;
+      }
+
+      //Last loop, fade out!
+      else if(k == rainbowLoops - 1 && j > 255 - fadeMax ){
+          fadeVal--;
+      }
+
+        strip.show();
+        delay(wait);
+    }
+  
+  }
+
+
+
+  delay(500);
+
+
+  for(int k = 0 ; k < whiteLoops ; k ++){
+
+    for(int j = 0; j < 256 ; j++){
+
+        for(uint16_t i=0; i < strip.numPixels(); i++) {
+            strip.setPixelColor(i, strip.Color(0,0,0, gamma[j] ) );
+          }
+          strip.show();
+        }
+
+        delay(2000);
+    for(int j = 255; j >= 0 ; j--){
+
+        for(uint16_t i=0; i < strip.numPixels(); i++) {
+            strip.setPixelColor(i, strip.Color(0,0,0, gamma[j] ) );
+          }
+          strip.show();
+        }
+  }
+
+  delay(500);
+
+
+}
+
+void whiteOverRainbow(uint8_t wait, uint8_t whiteSpeed, uint8_t whiteLength ) {
+  
+  if(whiteLength >= strip.numPixels()) whiteLength = strip.numPixels() - 1;
+
+  int head = whiteLength - 1;
+  int tail = 0;
+
+  int loops = 3;
+  int loopNum = 0;
+
+  static unsigned long lastTime = 0;
+
+
+  while(true){
+    for(int j=0; j<256; j++) {
+      for(uint16_t i=0; i<strip.numPixels(); i++) {
+        if((i >= tail && i <= head) || (tail > head && i >= tail) || (tail > head && i <= head) ){
+          strip.setPixelColor(i, strip.Color(0,0,0, 255 ) );
+        }
+        else{
+          strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+        }
+        
+      }
+
+      if(millis() - lastTime > whiteSpeed) {
+        head++;
+        tail++;
+        if(head == strip.numPixels()){
+          loopNum++;
+        }
+        lastTime = millis();
+      }
+
+      if(loopNum == loops) return;
+    
+      head%=strip.numPixels();
+      tail%=strip.numPixels();
+        strip.show();
+        delay(wait);
+    }
+  }
+  
+}
+void fullWhite() {
+  
+    for(uint16_t i=0; i<strip.numPixels(); i++) {
+        strip.setPixelColor(i, strip.Color(0,0,0, 255 ) );
+    }
+      strip.show();
+}
+
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256 * 5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3,0);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3,0);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0,0);
+}
+
+uint8_t red(uint32_t c) {
+  return (c >> 8);
+}
+uint8_t green(uint32_t c) {
+  return (c >> 16);
+}
+uint8_t blue(uint32_t c) {
+  return (c);
+}
+
+
+
+
+
